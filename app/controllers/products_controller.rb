d3a85043e41aa99f024df1b694f69603b64b83a4
le duong hien thong <michaeldiavolos@gmail.com>
2022-07-27 15:20:55 -0400

    fixed dollar sign formatting issue and added jpg by default

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,10 @@
 class ProductsController < ApplicationController
 
   def index
+    # uses model
     @products = Product.all.order(created_at: :desc)
+    # view
+    # render: index
   end
 
   def show
